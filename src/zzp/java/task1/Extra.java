b43110b9d5c5a2a3bb7d2f51ff12e541b44708be
dawid <dbielecki97@gmail.com>
2020-03-31 11:55:58 +0200

    implemented Builder pattern

---
+++ src/zzp/java/task1/Extra.java
@@ -0,0 +1,14 @@
+package zzp.java.task1;
+
+public class Extra {
+    private final String name;
+
+    public Extra(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/zzp/java/task1/SaucePosition.java
@@ -0,0 +1,5 @@
+package zzp.java.task1;
+
+public enum SaucePosition {
+    SAUCE_INSIDE, SAUCE_OUTSIDE;
+}
